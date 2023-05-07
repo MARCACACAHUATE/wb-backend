e631f263fa9473ecebd2341945b78da55186c8ed
MARCACACAHUATE <marcacacahuate@outlook.com>
2023-05-07 18:19:41 -0500

    Migraciones y Modelos para las entidades de Eventos

--- Models/Evento.cs
+++ Models/Evento.cs
@@ -7,16 +7,22 @@ namespace wb_backend.Models {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string? Nombre { get; set; }
-        public string? Tematica { get; set; }
+        public string? NombrePaquete { get; set; }
+        public string? Ocasion { get; set; }
+        public string? Servicios { get; set; }
+        public string? Mobiliario { get; set; }
+        public string ColorGlobos { get; set; } = "";
+        public float CostoEnvioMaterial { get; set; }
         [Required]
-        public string Direccion { get; set; } = "";
-        [Required]
-        public DateTime Fecha { get; set; }
+        public string Estado { get; set; } = "";
         [Required]
         public float Costo_reservacion { get; set; }
         [Required]
         public float Costo_total { get; set; }
+        public int Id_Municipio { get; set; }
+        [ForeignKey("Id_Municipio")]
+        public Municipio Municipio { get; set; } = null!;
+        public EventoSeparacion? Separacion { get; set; }
         public virtual ICollection<User> Users { get; set; } = new List<User>();
     }
 }
---
+++ Models/EventoSeparacion.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace wb_backend.Models {
+
+    public class EventoSeparacion {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        public string LastName { get; set; } = null!;
+        [Required]
+        public string Telefono { get; set; } = null!;
+        [Required]
+        public string Email { get; set; }  = null!;
+        public string HoraEvento { get; set; } = null!;
+        public string HoraMontaje { get; set; } = null!;
+        public DateTime Fecha { get; set; }
+        public string Calle { get; set; } = null!;
+        public string Numero { get; set; } = null!;
+        public string Colonia { get; set; } = null!;
+        public int Id_Evento { get; set; }
+        [ForeignKey("Id_Evento")]
+        public Evento Evento { get; set; } = null!; 
+    }
+}
